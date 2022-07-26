1e5b23dc00f43d0e7f5204a94aaf143ad6f6e16b
DSantos69 <david.csantos69@gmail.com>
2022-07-26 20:49:12 -0300

    initial config

---
+++ src/api/app.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import router from './controllers/index';
+import AppError from '@api/middlewares/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+app.use((error: Error, req: Request, res: Response) => {
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({
+            status: 'error',
+            message: error.message,
+        });
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'internal server error',
+    });
+});
+
+export { app };
